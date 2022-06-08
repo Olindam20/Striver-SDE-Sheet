ca1790418610c34d6f4d4ecec085499a7f6f1e7d
Olindam Sarma <olindamsarma2001@gmail.com>
2022-06-08 21:50:55 +0530

    Day1

---
+++ Arrays/Sort an array of 0’s 1’s 2’s.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo=0,mi=0,hi=nums.size()-1;
+        
+        while(mi<=hi)
+        {
+            if(nums[mi]==0)
+            {
+                swap(nums[lo],nums[mi]);
+                lo++;
+                mi++;
+            }
+            else if(nums[mi]==1)
+            {
+                mi++;
+            }
+            else 
+            {
+                swap(nums[mi],nums[hi]);
+                hi--;
+            }
+        }
+        
+    }
+};
